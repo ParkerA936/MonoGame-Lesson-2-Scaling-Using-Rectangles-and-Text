fdfcd8096e243c07e9e5978ce7a2001b67c558db
Parker Arnett <362581936@GoTVDSB.ca>
2026-04-13 13:12:38 -0400

    Done

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,13 @@ namespace MonoGame_Lesson_2___Scaling_Using_Rectangles_and_Text
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D circleTexture, rectangleTexture;
+        Rectangle circle1;
+        Rectangle rect1;
+        Rectangle topRect;
+        Rectangle Window;
+        Rectangle halfRect, centerRect, bottomRect;
+        SpriteFont textFont;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,7 +26,17 @@ namespace MonoGame_Lesson_2___Scaling_Using_Rectangles_and_Text
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            Window = new Rectangle(0,0,800,600);
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            rect1 = new Rectangle(50, 50, 200, 200);
+            circle1 = new Rectangle(50, 50, 200, 200);
+            topRect = new Rectangle(0,0, Window.Width/2, 50);
+            halfRect = new Rectangle(0, 0, Window.Width, 15);
+            centerRect = new Rectangle(Window.Width/2 - 25, Window.Height / 2 - 25, 50, 50);
+            bottomRect = new Rectangle(0, Window.Height - 20, Window.Width, 20);
+            _graphics.ApplyChanges();
+           
             base.Initialize();
         }
 
@@ -27,6 +44,10 @@ namespace MonoGame_Lesson_2___Scaling_Using_Rectangles_and_Text
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            circleTexture = Content.Load<Texture2D>("circle");
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
+
+            textFont = Content.Load<SpriteFont>("TextFont");
             // TODO: use this.Content to load your game content here
         }
 
@@ -45,6 +66,15 @@ namespace MonoGame_Lesson_2___Scaling_Using_Rectangles_and_Text
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(rectangleTexture,rect1,Color.Pink);
+            _spriteBatch.Draw(circleTexture, circle1 , Color.Blue);
+            _spriteBatch.Draw(rectangleTexture, topRect, Color.Yellow);
+            _spriteBatch.Draw(rectangleTexture, halfRect, Color.Green);
+            _spriteBatch.Draw(rectangleTexture,centerRect, Color.Red);
+            _spriteBatch.Draw(rectangleTexture, bottomRect, Color.Orange);
+            _spriteBatch.DrawString(textFont, "You Rule", new Vector2(80,280),Color.Black);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
